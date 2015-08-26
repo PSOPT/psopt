14083b0262e04992ab7158c2473df2743966b7ef
vmbecerra <vmbecerra99@gmail.com>
2015-08-26 08:32:59 -0700

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -40,7 +40,8 @@ sudo cp -r $HOME/adolc_base/include/* /usr/include/
 cd $HOME/Downloads
 wget --continue http://www.pdflib.com/binaries/PDFlib/705/PDFlib-Lite-7.0.5p3.tar.gz
 tar zxvf PDFlib-Lite-7.0.5p3.tar.gz
-cd PDFlib-Lite-7.0.5p3 $ ./configure
+cd PDFlib-Lite-7.0.5p3 
+./configure
 make; sudo make install
 sudo ldconfig
 # Download and install GNUplot
