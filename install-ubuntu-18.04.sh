f0c69e7947ad9a51571585926b479885b3c868fe
kordik <kordik@psyche4.itnet>
2020-03-04 10:05:53 +0100

    Installation script improvement
    This commit changes the installation script so that it is possible to change easily
    the names of downloaded third-party packages. This is achieved by introduction
    of variables of the archive file names. This change is very convenient for future
    updates of the packages.
    Moreover a new system of download and extract places is introduced. The packages
    are extracted into the PSOPT root directory by default. After that the directories
    of extracted packages are renamed from a complex versioned-name to a simple name
    (without the version number). The compressed package files are then  moved into a
    "Downloads" folder (created for that purpose in PSOPT root directory). According
    to the names of the packages in the "Downloads" folder it is possible to get the
    package version in the future.
    This new version of the install script prevents also Adol-c compilation to create
    a folder "adolc_base" in user home directory (which is really annoying). Instead of
    that this directory is created in Adol-c root directory.
    Personally I compiled PSOPT many times and the overall installation script never
    worked for me at the first attempt. There were always issues with compilation errors
    and it took a lot of manual work to adjust all paths and compiler flags etc. to went
    through the whole script.
    Based on this experience I made another modification, which makes the repairing procedure
    a bit more easier. After each compilation the user is asked, wheter some errors are
    shown in console. The advantage is that the user has now the information about what
    was just compiled and if there was a success or not. If there are some errors one
    can stop the script and try to repair the compilation errors.
    The next small change is that I commented gnuplot installation procedure and left it
    install by apt-get. For me it always worked the version from official repos. But if
    neccessary the comments can be reverted again.
    Apart from all that I needed for successful compilation of the dmatrix library to
    add "-fpermissive" compiler flag (my gcc version is 9.2.1). This worked with my Linux
    distro, which is a Debian sid. This amendment might not be neccessary with other
    distros and compiler versions.

--- install-ubuntu-18.04.sh
+++ install-ubuntu-18.04.sh
@@ -1,83 +1,126 @@
 # Script for installing PSOPT on Ubuntu 18.04
 # Install miscellaneous libraries
+
+export IPOPT_FILE=Ipopt-3.12.12.tgz
+export ADOL_FILE=ADOL-C-2.6.3.tgz
+export COLPACK_FILE=colpack_1.0.10.orig.tar.gz
+export PDFLIB_FILE=PDFlib-Lite-7.0.5p3.tar.gz
+export SUITESPARSE_FILE=SuiteSparse-4.4.3.tar.gz
+export LUSOL_FILE=lusol.zip
+
 export MYHOME=`pwd`
 echo $MYHOME
+mkdir Downloads
+
 sudo apt-get install build-essential pkg-config
 sudo apt-get install dh-autoreconf
 sudo apt-get -y install g++ gfortran f2c libf2c2-dev libf2c2 libblas-dev libopenblas-base libopenblas-dev libblas3 libatlas-base-dev liblapack-dev liblapack3
-# Download and install Ipopt, Metis and Mumps
-cd $HOME/Downloads
-wget --continue http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.12.tgz
-cd $HOME
-tar xzvf ./Downloads/Ipopt-3.12.12.tgz
-cd $HOME/Ipopt-3.12.12/ThirdParty/Metis
+echo -e '\n\n\e[33m \e[1mHit enter apt installation was succesfull. Otherwise press Ctrl-C and resolve arisen errors.\e[0m'
+read x
+
+####  Download and install Ipopt, Metis and Mumps #### 
+cd $MYHOME
+wget --continue http://www.coin-or.org/download/source/Ipopt/$IPOPT_FILE
+mkdir Ipopt && tar zxvf $IPOPT_FILE -C Ipopt --strip-components 1
+mv $IPOPT_FILE $MYHOME/Downloads
+cd $MYHOME/Ipopt/ThirdParty/Metis
 ./get.Metis
-cd $HOME/Ipopt-3.12.12/ThirdParty/Mumps
+cd $MYHOME/Ipopt/ThirdParty/Mumps
 ./get.Mumps
-cd $HOME/Ipopt-3.12.12
+cd $MYHOME/Ipopt
 ./configure --enable-static coin_skip_warn_cxxflags=yes
-make -j
+make -j4
 make install
-# Download and install ADOLC and ColPack
-cd $HOME/Downloads
-wget --continue www.coin-or.org/download/source/ADOL-C/ADOL-C-2.6.3.tgz
-cd $HOME
-tar zxvf ./Downloads/ADOL-C-2.6.3.tgz
-cd $HOME/ADOL-C-2.6.3
+echo -e '\n\n\e[33m \e[1mHit enter if compilation of Ipopt, Metis, and Mumps has been succesfull. Otherwise press Ctrl-C and resolve arisen errors.\e[0m'
+read x
+
+
+#### Download and install ADOLC and ColPack ####
+### First ColPack ### 
+cd $MYHOME
+wget --continue www.coin-or.org/download/source/ADOL-C/$ADOL_FILE
+mkdir Adol-c && tar zxvf $ADOL_FILE -C Adol-c --strip-components 1
+mv $ADOL_FILE $MYHOME/Downloads
+cd $MYHOME/Adol-c
+
+sed -i 's|export HOME=`pwd`||g' configure
+sed '2iexport HOME=`pwd`' configure > tmp.txt
+mv tmp.txt configure
+
 mkdir ./ThirdParty
 cd ./ThirdParty
-
-wget --continue http://archive.ubuntu.com/ubuntu/pool/universe/c/colpack/colpack_1.0.10.orig.tar.gz
-
-tar zxvf colpack_1.0.10.orig.tar.gz
-
-mv ColPack-1.0.10 ColPack
+wget --continue http://archive.ubuntu.com/ubuntu/pool/universe/c/colpack/$COLPACK_FILE
+mkdir ColPack && tar zxvf $COLPACK_FILE -C ColPack --strip-components 1
 cd ColPack
 ./autoconf.sh
 make
 sudo make install
-# sudo cp /usr/local/lib/libCol* /usr/lib
+echo -e '\n\n\e[33m \e[1mHit enter if compilation of Colpack has been succesfull. Otherwise press Ctrl-C and resolve arisen errors.\e[0m'
+read x
+
+
+### Second Adol ###
 sudo cp -P ./build/lib/libCol* /usr/lib
-cd $HOME/ADOL-C-2.6.3
-./configure --enable-sparse --with-colpack=$HOME/ADOL-C-2.6.3/ThirdParty/ColPack/build
+cd $MYHOME/Adol-c
+chmod +x configure
+./configure --enable-sparse --with-colpack=$MYHOME/Adol-c/ThirdParty/ColPack/build
 make
 make install
-sudo cp -P $HOME/adolc_base/lib64/lib* /usr/lib
-sudo cp -r $HOME/adolc_base/include/* /usr/include/
-# Download and install PDFlib
-cd $HOME/Downloads
-wget --continue https://fossies.org/linux/misc/old/PDFlib-Lite-7.0.5p3.tar.gz
-tar zxvf PDFlib-Lite-7.0.5p3.tar.gz
-cd PDFlib-Lite-7.0.5p3 
+sudo cp -P $MYHOME/Adol-c/adolc_base/lib64/lib* /usr/lib
+sudo cp -r $MYHOME/Adol-c/adolc_base/include/* /usr/include/
+echo -e '\n\n\e[33m \e[1mHit enter if compilation of Adol has been succesfull. Otherwise press Ctrl-C and resolve arisen errors.\e[0m'
+read x
+
+
+#### Download and install PDFlib ####
+cd $MYHOME
+wget --continue https://fossies.org/linux/misc/old/$PDFLIB_FILE
+mkdir PDFLib && tar zxvf $PDFLIB_FILE -C PDFLib --strip-components 1
+mv $PDFLIB_FILE $MYHOME/Downloads
+cd PDFLib
 ./configure
 make; sudo make install
 sudo ldconfig
-# Download and install GNUplot
-cd $HOME/Downloads
-wget --continue https://sourceforge.net/projects/gnuplot/files/gnuplot/4.4.0/gnuplot-4.4.0.tar.gz/download
-mv download gnuplot-4.4.0.tar.gz
-tar zxvf gnuplot-4.4.0.tar.gz
+echo -e '\n\n\e[33m \e[1mHit enter if compilation of PDFLib has been succesfull. Otherwise press Ctrl-C and resolve arisen errors.\e[0m'
+read x
+
+
+#### Download and install GNUplot ####
+# cd $MYHOME/Downloads
+# wget --continue https://sourceforge.net/projects/gnuplot/files/gnuplot/4.4.0/gnuplot-4.4.0.tar.gz/download
+# mv download gnuplot-4.4.0.tar.gz
+# tar zxvf gnuplot-4.4.0.tar.gz
 # sudo apt-get -y install libx11-dev libxt-dev libgd2-xpm-dev libreadline6-dev
-sudo apt-get -y install libx11-dev libxt-dev libreadline6-dev libgd-dev
-cd gnuplot-4.4.0
-./configure -with-readline=gnu -without-tutorial
-make;sudo make install
-# Download and extract PSOPT
-# cd $HOME
+sudo apt-get -y install libx11-dev libxt-dev libreadline6-dev libgd-dev gnuplot-qt
+# cd gnuplot-4.4.0
+# ./configure -with-readline=gnu -without-tutorial
+# make;sudo make install
+
+
+#### Download and extract PSOPT #### 
+# cd $MYHOME
 # wget --continue https://github.com/PSOPT/psopt/archive/master.zip
 # unzip master.zip
-# mv master.zip $HOME/Downloads
+# mv master.zip $MYHOME/Downloads
 cd $MYHOME
-# Download and extract SuiteSparse
-wget --continue http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.3.tar.gz
-tar zxvf SuiteSparse-4.4.3.tar.gz
+
+
+#### Download and extract SuiteSparse #### 
+wget --continue http://faculty.cse.tamu.edu/davis/SuiteSparse/$SUITESPARSE_FILE
+mkdir SuiteSparse && tar zxvf $SUITESPARSE_FILE -C SuiteSparse --strip-components 1
+mv $SUITESPARSE_FILE $MYHOME/Downloads
 cd $MYHOME
-# Download and extract LUSOL
-wget --continue http://www.stanford.edu/group/SOL/software/lusol/lusol.zip
-unzip lusol.zip
+
+
+#### Download and extract LUSOL #### 
+wget --continue http://www.stanford.edu/group/SOL/software/lusol/$LUSOL_FILE
+unzip $LUSOL_FILE
+mv $LUSOL_FILE $MYHOME/Downloads
 cd $MYHOME
-# Compile SuiteSparse, LUSOL, dmatrix and PSOPT
+
+
+####  Compile SuiteSparse, LUSOL, dmatrix and PSOPT #### 
 make all
-echo 'PSOPT installation script completed'
-echo 'PSOPT installed in $MYHOME'
+echo -e '\n\n\e[33m \e[1mPSOPT installation script completed\e[0m'
+echo        '\e[33m \e[1mPSOPT installed in \e[0m' $MYHOME 
 
