0ee4047038a3b1013540043b16fe8b756b8dc24c
Philipp Waxweiler <philipp@waxweiler.de>
2020-07-24 05:05:00 +0200

    added option to include examples. fixed example cmake lists

--- examples/genCMake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -uxe
-function create_cmake() {
-    local f="CMakeLists-generator.txt"
-    for d in $(ls -d */ | tr -d '/'); do
-        cat "$f" | sed -e "s/__MYDIR__/$d/g" > "$d/CMakeList.txt"
-    done
-}
-create_cmake
