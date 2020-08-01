e14688399d6f81260cb2e57513f95e6703c539f0
vmbecerra <vmbecerra99@gmail.com>
2020-08-01 13:23:21 +0100

    Update setup.cxx
    Updates related to change to Eigen3

--- src/setup.cxx
+++ src/setup.cxx
@@ -43,6 +43,7 @@ void psopt_level1_setup(Prob& problem)
        problem.phase[i].nsamples    = 0;
        problem.phase[i].zero_cost_integrand = false;
        problem.phase[i].regularization_factor = 0.0;
+       problem.phase[i].nodes.resize(1);
    }
 
 
@@ -66,48 +67,48 @@ void psopt_level2_setup(Prob& problem, Alg& algorithm)
 
 
 
-	problem.phase[i].scale.controls.Resize(ncontrols,1);
-	problem.phase[i].scale.states.Resize(nstates,1);
-	problem.phase[i].scale.defects.Resize(nstates,1);
-	problem.phase[i].scale.events.Resize(nevents,1);
-	problem.phase[i].scale.path.Resize(npath,1);
-        problem.phase[i].scale.parameters.Resize(nparam,1);
+	problem.phase[i].scale.controls.resize(ncontrols,1);
+	problem.phase[i].scale.states.resize(nstates,1);
+	problem.phase[i].scale.defects.resize(nstates,1);
+	problem.phase[i].scale.events.resize(nevents,1);
+	problem.phase[i].scale.path.resize(npath,1);
+   problem.phase[i].scale.parameters.resize(nparam,1);
 
-	problem.phase[i].bounds.upper.states.Resize(nstates,1);
-	problem.phase[i].bounds.lower.states.Resize(nstates,1);
-	problem.phase[i].bounds.lower.controls.Resize(ncontrols,1);
-	problem.phase[i].bounds.upper.controls.Resize(ncontrols,1);
-	problem.phase[i].bounds.upper.events.Resize(nevents,1);
-	problem.phase[i].bounds.lower.events.Resize(nevents,1);
-	problem.phase[i].bounds.upper.parameters.Resize(nparam,1);
-	problem.phase[i].bounds.lower.parameters.Resize(nparam,1);
-	problem.phase[i].bounds.lower.path.Resize(npath,1);
-	problem.phase[i].bounds.upper.path.Resize(npath,1);
+	problem.phase[i].bounds.upper.states.resize(nstates,1);
+	problem.phase[i].bounds.lower.states.resize(nstates,1);
+	problem.phase[i].bounds.lower.controls.resize(ncontrols,1);
+	problem.phase[i].bounds.upper.controls.resize(ncontrols,1);
+	problem.phase[i].bounds.upper.events.resize(nevents,1);
+	problem.phase[i].bounds.lower.events.resize(nevents,1);
+	problem.phase[i].bounds.upper.parameters.resize(nparam,1);
+	problem.phase[i].bounds.lower.parameters.resize(nparam,1);
+	problem.phase[i].bounds.lower.path.resize(npath,1);
+	problem.phase[i].bounds.upper.path.resize(npath,1);
 
 
-        problem.scale.linkages.Resize(nlinkages,1);
+   problem.scale.linkages.resize(nlinkages,1);
 
-        problem.phase[i].guess.controls.Resize(0,0);
-        problem.phase[i].guess.states.Resize(0,0);
-        problem.phase[i].guess.time.Resize(0,0);
-        problem.phase[i].guess.parameters.Resize(0,0);
+   problem.phase[i].guess.controls.resize(0,0);
+   problem.phase[i].guess.states.resize(0,0);
+   problem.phase[i].guess.time.resize(0,0);
+   problem.phase[i].guess.parameters.resize(0,0);
 
-        problem.phase[i].name.states_ = new string[nstates];
-        problem.phase[i].name.controls_ = new string[ncontrols];
-        problem.phase[i].name.parameters_ = new string[nparam];
-        problem.phase[i].units.states_  = new string[nstates];
-        problem.phase[i].units.controls_ = new string[ncontrols];
-        problem.phase[i].units.parameters_ = new string[nparam];
+   problem.phase[i].name.states_ = new string[nstates];
+   problem.phase[i].name.controls_ = new string[ncontrols];
+   problem.phase[i].name.parameters_ = new string[nparam];
+   problem.phase[i].units.states_  = new string[nstates];
+   problem.phase[i].units.controls_ = new string[ncontrols];
+   problem.phase[i].units.parameters_ = new string[nparam];
 
 
 
   }
 
-  problem.bounds.lower.linkage.Resize(nlinkages,1);
-  problem.bounds.upper.linkage.Resize(nlinkages,1);
+  problem.bounds.lower.linkage.resize(nlinkages,1);
+  problem.bounds.upper.linkage.resize(nlinkages,1);
 
-  problem.bounds.lower.linkage.FillWithZeros();
-  problem.bounds.lower.linkage.FillWithZeros();
+  problem.bounds.lower.linkage.setZero();
+  problem.bounds.lower.linkage.setZero();
 
 
   problem.endpoint_cost               = NULL;
@@ -127,29 +128,27 @@ void psopt_level2_setup(Prob& problem, Alg& algorithm)
   algorithm.constraint_scaling          = "automatic";
   algorithm.nlp_iter_max                = 1000;
   algorithm.nlp_tolerance               = 1.e-6;
-  algorithm.jac_sparsity_ratio  	= 0.5;
-  algorithm.hess_sparsity_ratio 	= 0.2;
+  algorithm.jac_sparsity_ratio  	       = 0.5;
+  algorithm.hess_sparsity_ratio 	       = 0.2;
   algorithm.hessian                     = "limited-memory";
   algorithm.collocation_method          = "Legendre";
   algorithm.diff_matrix                 = "standard";
   algorithm.print_level                 = 1;
   algorithm.save_sparsity_pattern       = 0;
   algorithm.nsteps_error_integration    = 10;
-  algorithm.ode_tolerance                = 1.e-3;
+  algorithm.ode_tolerance               = 1.e-3;
   algorithm.mr_max_increment_factor     = 0.4;
-  algorithm.mr_max_iterations		= 7;
+  algorithm.mr_max_iterations		       = 7;
   algorithm.mr_min_extrapolation_points = 2;
   algorithm.mr_initial_increment        = 5;
   algorithm.mr_kappa                    = 0.1;
   algorithm.mr_M1                       = 5;
-  algorithm.mesh_refinement 		= "manual";
+  algorithm.mesh_refinement 		       = "manual";
   algorithm.switch_order                = 2;
   algorithm.parameter_statistics        = "yes";
   algorithm.parameter_estimation_norm   = 2;
   algorithm.ipopt_max_cpu_time          = 3600.0;
-
-
-  problem.multi_segment_flag = false;
+  problem.multi_segment_flag            = false;
 
 
   return;
