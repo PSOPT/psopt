582da40b773a6ec2d29c25b58f1f4d51f84f53b1
vmbecerra <vmbecerra99@gmail.com>
2020-08-01 11:49:12 +0100

    Update NLP_bounds.cxx
    Updates related to change to Eigen3

--- src/NLP_bounds.cxx
+++ src/NLP_bounds.cxx
@@ -33,7 +33,8 @@ e-mail:    v.m.becerra@ieee.org
 
 
 
-void  define_nlp_bounds(DMatrix& xlb, DMatrix& xub, Prob& problem, Alg& algorithm, Workspace* workspace)
+
+void  define_nlp_bounds(MatrixXd& xlb, MatrixXd& xub, Prob& problem, Alg& algorithm, Workspace* workspace)
 {
   // This function defines the NLP bounds given the information provided by
   // the user.
@@ -50,57 +51,70 @@ void  define_nlp_bounds(DMatrix& xlb, DMatrix& xub, Prob& problem, Alg& algorith
    {
    	int norder    = problem.phase[i].current_number_of_intervals;
    	int ncontrols = problem.phase[i].ncontrols;
-        int nparam    = problem.phase[i].nparameters;
+    int nparam    = problem.phase[i].nparameters;
    	int nstates   = problem.phase[i].nstates;
    	int offset1   = ncontrols*(norder+1);
-        int offset2   = (ncontrols+nstates)*(norder+1);
+    int offset2   = (ncontrols+nstates)*(norder+1);
 
-	DMatrix& control_scaling = problem.phase[i].scale.controls;
-	DMatrix& state_scaling   = problem.phase[i].scale.states;
-        DMatrix& param_scaling   = problem.phase[i].scale.parameters;
-        double time_scaling      =  problem.phase[i].scale.time;
+	MatrixXd& control_scaling = problem.phase[i].scale.controls;
+	MatrixXd& state_scaling   = problem.phase[i].scale.states;
+    MatrixXd& param_scaling   = problem.phase[i].scale.parameters;
+    double time_scaling      = problem.phase[i].scale.time;
 
 	int nvars_phase_i = get_nvars_phase_i(problem,i, workspace);
 
 
-	for (k=1; k<=norder+1; k++) {
+	for (k=0; k<norder+1; k++) {   // EIGEN_UPDATE: k index shifted by -1.
                 if (ncontrols>0) {
-	              xlb(colon(x_phase_offset+(k-1)*ncontrols+1,x_phase_offset+ k*ncontrols) ) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
+//	              xlb(colon(x_phase_offset+(k-1)*ncontrols+1,x_phase_offset+ (k)*ncontrols) ) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
+	              xlb.block(x_phase_offset+k*ncontrols, 0, ncontrols, 1 ) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
                 }
-		xlb(colon(x_phase_offset+(k-1)*nstates+1+offset1, x_phase_offset+k*nstates+offset1))=elemProduct((problem.phase[i].bounds.lower.states),state_scaling);
+//		xlb(colon(x_phase_offset+(k-1)*nstates+1+offset1, x_phase_offset+(k)*nstates+offset1))=elemProduct((problem.phase[i].bounds.lower.states),state_scaling);
+		xlb.block(x_phase_offset+(k)*nstates+offset1,0, nstates, 1) = elemProduct((problem.phase[i].bounds.lower.states),state_scaling);
 
                 if (ncontrols>0) {
-		     xub(colon(x_phase_offset+(k-1)*ncontrols+1, x_phase_offset+k*ncontrols) ) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+//		     xub(colon(x_phase_offset+(k-1)*ncontrols+1, x_phase_offset+(k)*ncontrols) ) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+		     xub.block(x_phase_offset+(k)*ncontrols,0, ncontrols, 1) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+
                 }
-		xub(colon(x_phase_offset+(k-1)*nstates+1+offset1, x_phase_offset+k*nstates+offset1))=elemProduct((problem.phase[i].bounds.upper.states),state_scaling);
+//		xub(colon(x_phase_offset+(k-1)*nstates+1+offset1, x_phase_offset+(k)*nstates+offset1))=elemProduct((problem.phase[i].bounds.upper.states),state_scaling);
+        xub.block(x_phase_offset+(k)*nstates+offset1,0, nstates, 1)=elemProduct((problem.phase[i].bounds.upper.states),state_scaling);
+
 	}
 
         offset1 = (nstates+ncontrols)*(norder+1);
 
         if (nparam>=1) {
 
-           xlb(colon(x_phase_offset+offset2+1, x_phase_offset+offset2+nparam)) = elemProduct((problem.phase[i].bounds.lower.parameters),param_scaling);
+ //          xlb(colon(x_phase_offset+offset2+1, x_phase_offset+offset2+nparam)) = elemProduct((problem.phase[i].bounds.lower.parameters),param_scaling);
+             xlb.block(x_phase_offset+offset2,0,nparam,1)= elemProduct((problem.phase[i].bounds.lower.parameters),param_scaling);
 
-           xub(colon(x_phase_offset+offset2+1, x_phase_offset+offset2+nparam)) = elemProduct((problem.phase[i].bounds.upper.parameters),param_scaling);
+
+//           xub(colon(x_phase_offset+offset2+1, x_phase_offset+offset2+nparam)) = elemProduct((problem.phase[i].bounds.upper.parameters),param_scaling);
+             xub.block(x_phase_offset+offset2,0,nparam,1)= elemProduct((problem.phase[i].bounds.upper.parameters),param_scaling);
 
         }
 
         offset1 = (nstates+ncontrols)*(norder+1) + nparam;
 
         if ( need_midpoint_controls(*workspace->algorithm, workspace) ) {
-	   for (k=1; k<=norder; k++) {
+	   for (k=0; k<norder; k++) { // EIGEN_UPDATE: K index shifted by -1
                 if (ncontrols>0) {
-	              xlb(colon(x_phase_offset+offset1+(k-1)*ncontrols+1,x_phase_offset+offset1+ k*ncontrols) ) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
-		     xub(colon(x_phase_offset+offset1+(k-1)*ncontrols+1, x_phase_offset+offset1+k*ncontrols) ) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+//	              xlb(colon(x_phase_offset+offset1+(k-1)*ncontrols+1,x_phase_offset+offset1+ k*ncontrols) ) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
+		          xlb.block(x_phase_offset+offset1+(k)*ncontrols,0,ncontrols,1) = elemProduct((problem.phase[i].bounds.lower.controls),control_scaling);
+
+//		          xub(colon(x_phase_offset+offset1+(k-1)*ncontrols+1, x_phase_offset+offset1+k*ncontrols) ) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+                  xub.block(x_phase_offset+offset1+(k)*ncontrols,0,ncontrols,1) = elemProduct((problem.phase[i].bounds.upper.controls),control_scaling);
+
                 }
 	   }
         }
 
-	xlb(x_phase_offset+nvars_phase_i-1)   = problem.phase[i].bounds.lower.StartTime*time_scaling;
-	xub(x_phase_offset+nvars_phase_i-1)   = problem.phase[i].bounds.upper.StartTime*time_scaling;
+	xlb(x_phase_offset+nvars_phase_i-2)   = problem.phase[i].bounds.lower.StartTime*time_scaling; //EIGEN_UPDATE
+	xub(x_phase_offset+nvars_phase_i-2)   = problem.phase[i].bounds.upper.StartTime*time_scaling; //EIGEN_UPDATE
 
-	xlb(x_phase_offset+nvars_phase_i) = problem.phase[i].bounds.lower.EndTime*time_scaling;
-	xub(x_phase_offset+nvars_phase_i) = problem.phase[i].bounds.upper.EndTime*time_scaling;
+	xlb(x_phase_offset+nvars_phase_i-1) = problem.phase[i].bounds.lower.EndTime*time_scaling;  //EIGEN_UPDATE
+	xub(x_phase_offset+nvars_phase_i-1) = problem.phase[i].bounds.upper.EndTime*time_scaling;  //EIGEN_UPDATE
 
         x_phase_offset += nvars_phase_i;
 
@@ -120,7 +134,7 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
 
   int lam_phase_offset=0;
 
-  DMatrix& constraint_scaling = *workspace->constraint_scaling;
+  MatrixXd& constraint_scaling = *workspace->constraint_scaling;
 
   int offset, k, l, i, j;
 
@@ -132,8 +146,8 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
   	int norder  = problem->phase[i].current_number_of_intervals;
   	int nevents = problem->phase[i].nevents;
   	int npath   = problem->phase[i].npath;
-  	DMatrix& path_scaling    = problem->phase[i].scale.path;
-  	DMatrix& event_scaling   = problem->phase[i].scale.events;
+  	MatrixXd& path_scaling    = problem->phase[i].scale.path;
+  	MatrixXd& event_scaling   = problem->phase[i].scale.events;
 
         int ncons_phase_i = get_ncons_phase_i(*problem,i, workspace);
 	// lower and upper bounds on g(x)
@@ -144,12 +158,12 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
 
 	offset = lam_phase_offset+nstates*(norder+1);
 
-	for (k=1;k<=nevents;k++) {
-		j = offset + k-1;
+	for (k=0;k<nevents;k++) { // EIGEN_UPDATE: index k shifted by -1
+		j = offset + k;
 		if( algorithm->scaling=="user" )
 		        event_sc = event_scaling(k);
  		else
-			event_sc = constraint_scaling(j+1);
+			event_sc = constraint_scaling(j);
 
 		g_l[j] = (problem->phase[i].bounds.lower.events)(k)*event_sc;
 		g_u[j] = (problem->phase[i].bounds.upper.events)(k)*event_sc;
@@ -157,14 +171,14 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
 
 	offset = offset + nevents;
 
-	for (k=1; k<=npath; k++)
+	for (k=0; k<npath; k++) // EIGEN_UPDATE: index k shifted by -1
 	{
-		for (l=1;l<=(norder + 1);l++) {
-		    j = offset + (l-1)*npath + k - 1;
+		for (l=0;l<(norder + 1);l++) {   // EIGEN_UPDATE: index l shifted by -1.
+		    j = offset + (l)*npath + k - 1;
 		    if( algorithm->scaling=="user" )
 		       path_sc = path_scaling(k);
 		    else
-		       path_sc = constraint_scaling(j+1);
+		       path_sc = constraint_scaling(j);
 
 		    g_l[j] = (problem->phase[i].bounds.lower.path)(k)*path_sc;
 		    g_u[j] = (problem->phase[i].bounds.upper.path)(k)*path_sc;
@@ -175,14 +189,14 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
         if ( need_midpoint_controls(*workspace->algorithm, workspace) ) {
 		offset = offset + npath*(norder+1);
 
-		for (k=1; k<=npath; k++)
+		for (k=0; k<npath; k++)  // EIGEN_UPDATE: index k shifted by -1.
 		{
-			for (l=1;l<=norder;l++) {
-		    		j = offset + (l-1)*npath + k - 1;
+			for (l=0;l<norder;l++) { // EIGEN_UPDATE: index l shifted by -1.
+		    		j = offset + (l)*npath + k - 1;
 		    		if( algorithm->scaling=="user" )
 		       			path_sc = path_scaling(k);
 		    		else
-		       			path_sc = constraint_scaling(j+1);
+		       			path_sc = constraint_scaling(j);
 
 		    		g_l[j] = (problem->phase[i].bounds.lower.path)(k)*path_sc;
 		    		g_u[j] = (problem->phase[i].bounds.upper.path)(k)*path_sc;
@@ -198,7 +212,7 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
         double diff_t0Min_tfMax= (problem->phase[i].bounds.lower.StartTime-problem->phase[i].bounds.upper.EndTime);
         diff_t0Min_tfMax *= problem->phase[i].scale.time;
         if (algorithm->scaling == "automatic")  {
-           diff_t0Min_tfMax *= constraint_scaling(lam_phase_offset);
+           diff_t0Min_tfMax *= constraint_scaling(lam_phase_offset-1);
         }
 
         g_l[ lam_phase_offset - 1 ] = diff_t0Min_tfMax;
@@ -209,12 +223,13 @@ void get_constraint_bounds(double* g_l, double* g_u, Workspace* workspace)
   for (k=0;k<problem->nlinkages; k++)
   {
         j = lam_phase_offset + k;
-        g_l[j] = problem->bounds.lower.linkage(k+1);
-        g_u[j] = problem->bounds.upper.linkage(k+1);
+        g_l[j] = problem->bounds.lower.linkage(k); // EIGEN_UPDATE
+        g_u[j] = problem->bounds.upper.linkage(k); // EIGEN_UPDATE
 
   }
 
 
   return;
 }
+
 
