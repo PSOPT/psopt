30e2492ba553493ef0741a146c67a588d919ce46
Victor Becerra <vmbecerra99@gmail.com>
2020-08-06 08:37:41 +0100

    Updated example

--- examples/heat/heat.cxx
+++ examples/heat/heat.cxx
@@ -51,9 +51,9 @@ adouble integrand_cost(adouble* states, adouble* controls, adouble* parameters,
 
      adouble yd = 2.0-exp(rho*time);
  
-     adouble yN = states[ CINDEX(N) ];
+     adouble yN = states[ N-1 ];
 
-     adouble v1 = controls[ CINDEX(1) ];
+     adouble v1 = controls[ 0 ];
 
      return   0.5*(  pow(yN-yd,2.0) + gamma*pow(v1,2.0) );
 } 
@@ -89,14 +89,14 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
 
    adouble* y 		= states;
 
-   adouble v1 		= controls[ CINDEX(1) ];
-   adouble v2 		= controls[ CINDEX(2) ];
-   adouble v3 		= controls[ CINDEX(3) ];
+   adouble v1 		= controls[ 0 ];
+   adouble v2 		= controls[ 1 ];
+   adouble v3 		= controls[ 2 ];
 
-   adouble y1 		= y[CINDEX(1)];
-   adouble y2 		= y[CINDEX(2)];
-   adouble yN 		= y[CINDEX(N)];
-   adouble yNm1 	= y[CINDEX(N-1)];
+   adouble y1 		= y[ 0 ];
+   adouble y2 		= y[ 1 ];
+   adouble yN 		= y[ N-1 ];
+   adouble yNm1 	= y[ N-2 ];
 
    adouble x1 		=   0;
    adouble xN 		=   1;
@@ -106,27 +106,27 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
    adouble qN =  (rho*(a1+2*a2) + pi*pi*(a3+2*a4))*exp(rho*time)*cos(pi*xN) 
                      - a4*pi*pi*exp(2*rho*time) + (2*a4*pi*pi+rho*a2)*exp(2*rho*time)*pow(cos(pi*xN),2.0);
 
-   derivatives[ CINDEX(1) ] = 1.0/(a1+a2*y1)*(q1 + 
+   derivatives[ 0 ] = 1.0/(a1+a2*y1)*(q1 + 
                               (1.0/pow(delta,2.0))*(a3+a4*y1)*(y2-2*y1+v2)+a4*pow( (y2-v2)/(2*delta), 2.0) );
   
-   for(i=2;i<=(N-1);i++) {
-       adouble yi   = y[CINDEX(i)];
-       adouble yim1 = y[CINDEX(i-1)];
-       adouble yip1 = y[CINDEX(i+1)];
-       adouble xi =   ( (double) (i-1) ) /( (double) (N-1) );
+   for(i=1;i<(N-1);i++) {
+       adouble yi   = y[i];
+       adouble yim1 = y[i-1];
+       adouble yip1 = y[i+1];
+       adouble xi =   ( (double) (i) ) /( (double) (N-1) );
        adouble qi = (rho*(a1+2*a2) + pi*pi*(a3+2*a4))*exp(rho*time)*cos(pi*xi) 
                      - a4*pi*pi*exp(2*rho*time) + (2*a4*pi*pi+rho*a2)*exp(2*rho*time)*pow(cos(pi*xi),2.0);
 
-       derivatives[CINDEX(i)]=1.0/(a1+a2*yi)*(qi + (1.0/pow(delta,2.0))*
+       derivatives[i]=1.0/(a1+a2*yi)*(qi + (1.0/pow(delta,2.0))*
                               (a3+a4*yi)*(yip1-2*yi+yim1)+a4*pow( (yip1-yim1)/(2*delta), 2.0) );
    }
 
-   derivatives[ CINDEX(N) ] = 1.0/(a1+a2*yN)*(qN + (1.0/pow(delta,2.0))*
+   derivatives[ N-1 ] = 1.0/(a1+a2*yN)*(qN + (1.0/pow(delta,2.0))*
                               (a3+a4*yN)*(v3-2*yN+yNm1)+a4*pow( (v3-yNm1)/(2*delta), 2.0) );
 
-   path[CINDEX(1)] =  g*(y1-v1) - (1.0/(2*delta))*(a3+a4*y1)*(y2-v2); 
+   path[ 0 ] =  g*(y1-v1) - (1.0/(2*delta))*(a3+a4*y1)*(y2-v2); 
 
-   path[CINDEX(2)] = (1.0/(2*delta))*(a3+a4*yN)*(v3-yNm1);
+   path[ 1 ] = (1.0/(2*delta))*(a3+a4*yN)*(v3-yNm1);
 
 }
 
@@ -144,8 +144,8 @@ void events(adouble* e, adouble* initial_states, adouble* final_states,
 
    int N = N_DISCRETIZATION;
 
-   for(i=1;i<= N; i++) {
-          e[CINDEX(i)] = initial_states[CINDEX(i)];
+   for(i=0;i< N; i++) {
+          e[ i ] = initial_states[ i ];
    }
 
 }
@@ -193,8 +193,8 @@ int main(void)
 ////////////  Define problem level constants & do level 1 setup ////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    problem.nphases   			= 1;
-    problem.nlinkages                   = 0;
+    problem.nphases   					= 1;
+    problem.nlinkages               = 0;
 
     psopt_level1_setup(problem);
 
@@ -206,7 +206,7 @@ int main(void)
     problem.phases(1).ncontrols 		= 3;
     problem.phases(1).nevents   		= N;
     problem.phases(1).npath     		= 2;
-    problem.phases(1).nodes                     = "[20 50 60]"; 
+    problem.phases(1).nodes         = (RowVectorXi(3)<< 20, 50, 60 ).finished();            //  "[20 50 60]"; 
 
 
     psopt_level2_setup(problem, algorithm);
@@ -224,17 +224,13 @@ int main(void)
     problem.phases(1).bounds.upper.states = 3.0*ones(N,1);
 
 
-    problem.phases(1).bounds.lower.controls(1) = 0.0;
-    problem.phases(1).bounds.lower.controls(2) = 0.0;
-    problem.phases(1).bounds.lower.controls(3) = 0.0;
+    problem.phases(1).bounds.lower.controls <<  0.0, 0.0, 0.0;
+
+    problem.phases(1).bounds.upper.controls << 0.1, 3.0, 3.0;
 
 
-    problem.phases(1).bounds.upper.controls(1) = 0.1;
-    problem.phases(1).bounds.upper.controls(2) = 3.0;
-    problem.phases(1).bounds.upper.controls(3) = 3.0;
-
-    for(i = 1; i<= N; i++ ) {
-        double xi =    ( (double) (i-1) ) /( (double) (N-1) );
+    for(i = 0; i< N; i++ ) {
+        double xi =    ( (double) (i) ) /( (double) (N-1) );
     	double yI = 2.0+cos(pi*xi);
 
     	problem.phases(1).bounds.lower.events(i) = yI;
@@ -242,13 +238,9 @@ int main(void)
 
     }
 
+    problem.phases(1).bounds.lower.path << 0.0, 0.0;
 
-    problem.phases(1).bounds.upper.path(1) = 0.0;
-    problem.phases(1).bounds.upper.path(2) = 0.0;
-    problem.phases(1).bounds.lower.path(1) = 0.0;
-    problem.phases(1).bounds.lower.path(2) = 0.0;
-
-
+    problem.phases(1).bounds.upper.path << 0.0, 0.0;
 
     problem.phases(1).bounds.lower.StartTime    = 0.0;
     problem.phases(1).bounds.upper.StartTime    = 0.0;
@@ -272,24 +264,24 @@ int main(void)
 ///////////////////  Define & register initial guess ///////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    int nnodes    			= problem.phases(1).nodes(1);
+    int nnodes    						    = problem.phases(1).nodes(0);
     int ncontrols                       = problem.phases(1).ncontrols;
     int nstates                         = problem.phases(1).nstates;
 
-    DMatrix state_guess    =  zeros(nstates,nnodes);
-    DMatrix control_guess  =  zeros(ncontrols,nnodes);
-    DMatrix param_guess    =  zeros(0,0);
-    DMatrix time_guess     =  linspace(0,0.5,nnodes);
+    MatrixXd state_guess    =  zeros(nstates,nnodes);
+    MatrixXd control_guess  =  zeros(ncontrols,nnodes);
+    MatrixXd param_guess    =  zeros(0,0);
+    MatrixXd time_guess     =  linspace(0,0.5,nnodes);
 
-    for(i = 1; i<= N; i++ ) {
-        double xi =    ( (double) (i-1) ) /( (double) (N-1) );
+    for(i = 0; i< N; i++ ) {
+        double xi =    ( (double) (i) ) /( (double) (N-1) );
     	double yI = 2.0+cos(pi*xi);
-    	state_guess(i,colon()) = yI*ones(1,nnodes);
+    	state_guess.row(i) = yI*ones(1,nnodes);
     }
 
-    control_guess(1,colon()) = linspace(0.1,0.1, nnodes);
-    control_guess(2,colon()) = state_guess(1,colon());
-    control_guess(3,colon()) = state_guess(N,colon());
+    control_guess <<  linspace(0.1,0.1, nnodes);
+                      state_guess.row(0), 
+                      state_guess.row(N-1);
 
 
     auto_phase_guess(problem, control_guess, state_guess, param_guess, time_guess);
@@ -304,7 +296,7 @@ int main(void)
     algorithm.nlp_method                  = "IPOPT";
     algorithm.scaling                     = "automatic";
     algorithm.derivatives                 = "automatic";
-    algorithm.defect_scaling              = "jacobian-based";    
+//    algorithm.defect_scaling              = "jacobian-based";    
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Now call PSOPT to solve the problem   //////////////////
@@ -317,7 +309,7 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////
 
 
-    DMatrix y, u, t;
+    MatrixXd y, u, t;
 
     y      = solution.get_states_in_phase(1);
     u      = solution.get_controls_in_phase(1);
@@ -328,14 +320,14 @@ int main(void)
 ///////////  Save solution data to files if desired ////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    y.Save("y.dat");
-    u.Save("u.dat");
-    t.Save("t.dat");    
+    Save(y,"y.dat");
+    Save(u,"u.dat");
+    Save(t,"t.dat");    
 
-    DMatrix x(N);
+    MatrixXd x(N,1);
 
-    for(i = 1; i<= N; i++ ) {
-        x(i) =    ( (double) (i-1) ) /( (double) (N-1) );
+    for(i = 0; i< N; i++ ) {
+        x(i) =    ( (double) (i) ) /( (double) (N-1) );
     }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -343,19 +335,19 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////
 
 
-    plot(t,u(1,colon()),problem.name+": control","time (s)", "control", "u");
+    plot(t,u.row(0),problem.name+": control","time (s)", "control", "u");
 
-    plot(t,u(1,colon()),problem.name+": control","time (s)", "control", "v1", 
+    plot(t,u.row(0),problem.name+": control","time (s)", "control", "v1", 
 			"pdf", "heat_control.pdf");
 
-    plot(t,y(1,colon()),problem.name+": state","time (s)", "state", "x1");
+    plot(t,y.row(0) ,problem.name+": state","time (s)", "state", "x1");
 
-    plot(t,y(1,colon()),problem.name+": state","time (s)", "state", "x1",
+    plot(t,y.row(0) ,problem.name+": state","time (s)", "state", "x1",
 			"pdf", "heat_state1.pdf");
 
-    plot(t,y(N,colon()),problem.name+": state","time (s)", "state", "xN");
+    plot(t,y.row(N-1) ,problem.name+": state","time (s)", "state", "xN");
 
-    plot(t,y(N,colon()),problem.name+": state","time (s)", "state", "xN",
+    plot(t,y.row(N-1) ,problem.name+": state","time (s)", "state", "xN",
 			"pdf", "heat_stateN.pdf");
 
     surf(x, t,  y, problem.name, "x", "t", "h"); 
